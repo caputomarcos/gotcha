8c8d4821fb25629a9639298b2bf11e64df5d01da
Marcos Caputo <caputo.marcos@gmail.com>
2022-10-08 23:27:57 -0300

    fix  FileNotFoundError: [Errno 2] No such file or directory #2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """setup GOTCHA_TTY
 """
-__updated__ = "2022-10-08 20:58:57"
+__updated__ = "2022-10-08 23:24:42"
 
 import os
 from configparser import ConfigParser
@@ -101,6 +101,7 @@ setup(
     license="MIT",
     install_requires=[
         "psutil==5.9.2",
+        "pyparsing==3.0.8"
     ],
     entry_points={
         "console_scripts": [
